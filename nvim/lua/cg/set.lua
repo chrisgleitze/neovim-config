de1e84f8cf65daf7320ec954f1a4e49b9f11d59b
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-06-12 00:25:55 +0200

    add set.lua file for relative line numbers etc.

---
+++ nvim/lua/cg/set.lua
@@ -0,0 +1,7 @@
+-- set absolute number for current line, relative numbers for all other lines
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- no swapfile creation, no backup files
+vim.opt.swapfile = false
+vim.opt.backup = false
