0a1cceec739f09250776d387ac938f523911c8c5
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-06-12 01:33:53 +0200

    add more remaps

--- nvim/lua/cg/remap.lua
+++ nvim/lua/cg/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
+
+-- exit file
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- moves highlighted lines up (K) or down (J)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- helps you change all occurrences of the word the cursor is on
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
