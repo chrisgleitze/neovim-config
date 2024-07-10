f20e656bf9e6452a485304a3bab9e03861700f6e
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-07-10 12:04:14 +0200

    add path completion fix, new harpoon keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 require("cg.init")
+
 require("cg.packer")
 require("cg.remap")
 require("cg.set")
@@ -19,3 +20,15 @@ require("after.plugin.nvim-cmp")
 require("after.plugin.telescope")
 require("after.plugin.treesitter")
 require("after.plugin.undotree")
+
+vim.g.completion_auto_change_source = true
+vim.g.completion_chain_complete_list = {
+  default = {
+    default = {
+      { complete_items = {'lsp', 'snippets'} },
+    },
+    string = {
+      { mode = 'file' },  -- use ins-complete "files" (search ins-compl in completion-nvim help)
+    },
+  },
+}
