7f8befe5581ca50cb6c137a63225bbaf245cb11d
Christian Gleitze <87121518+chrisgleitze@users.noreply.github.com>
2024-07-18 12:55:26 +0200

    change to harpoon keymaps

--- nvim/lua/cg/lsp/mason.lua
+++ nvim/lua/cg/lsp/mason.lua
@@ -22,13 +22,14 @@ mason.setup()
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"tsserver",
-		"html",
 		"cssls",
-		"tailwindcss",
-		"lua_ls",
 		"emmet_ls",
+		"html",
+		"jsonls",
+		"lua_ls",
 		"rust_analyzer",
+		"tailwindcss",
+		"tsserver",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
